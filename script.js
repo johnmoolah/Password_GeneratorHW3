925c17da020636cfe6f68880f1a95717acd41c60
John Delizo <johndelizo99@gmail.com>
2020-10-19 19:21:56 -0700

    Set slider to default of 25, max to 50, min of 1; User able to copy generated password to clipboard

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-//Generate Random Password
+//generate Random Password
 function generate(){
 
     //set password length/complexity
     let complexity = document.getElementById("slider").value;
     
     //possible password values
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()";
 
     let password = "";
 
     //create for loop to choose password characters
-    for(var i = 0; i <= complexity; i++){
-        password = password = values.charAct(Math.floor(Math.random() * Math.floor(value.length - 1)));
+    for(var i = 0; i <= complexity; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
 
         //add password to textbox/display area
@@ -20,5 +20,27 @@ function generate(){
         //add password to previously generated passwords
         document.getElementById("lastNums").innerHTML += password + "<br />";
 
+}
+//set default length display of 25
+document.getElementById("length").innerHTML = "length: 25";
 
+//function to set length based on slider position
+document.getElementById("slider").oninput = function() {
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "length: 1";
+    }
+}
+
+//funtion to copy password to clipboard
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+    
+    alert("Copied to clipboard");
 }
